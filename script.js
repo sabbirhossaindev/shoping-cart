863ea8f43b5a554c8e3d2aa843b477191672e664
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-19 01:45:55 +0600

    updateCaseNumber

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+
+function updateCaseNumber(isIncrease) {
+     const caseNumberField = document.getElementById('case-number-field');
+     const caseNumberString = caseNumberField.value;
+     const previousCaseNumber = parseInt(caseNumberString);
+    let newCaseNumber;
+    if (isIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
 document.getElementById('btn-case-plus').addEventListener('click', function () {
-    // kno akta input value ka niya asa 
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
-
-    // value sat case Number Field
-    const newCaseNumber = previousCaseNumber + 1;
-    caseNumberField.value = newCaseNumber;
+    const newCaseNumber = updateCaseNumber(true);
+    
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
 });
-// case -number - minus
-document.getElementById('case-number-minus').addEventListener('click', function () {
-    // kno akta input value ka niya asa 
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
 
-    // value sat case Number Field
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberField.value = newCaseNumber;
+document.getElementById('case-number-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 });
