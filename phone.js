da9e0a2d998061fb8a101faafa99010683f05d17
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-19 14:31:45 +0600

    updateCaseNumber

--- phone.js
+++ phone.js
@@ -17,13 +17,31 @@ function updatePhoneNumber(isIncrease) {
     return newPhoneNumber;
    }
 
+//
+function updatePhoneTotalPrice(newPhoneNumber) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+function getElementValueById(elementId) {
+    const phoneTotalElement = document.getElementById(elementId);
+    const currentPhoneTotalString = phoneTotalElement.innerText;
+    const currentPhoneTotal = parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
+}
 
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
-    updatePhoneNumber(true);
-    
+    const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneTotalPrice(newPhoneNumber);
+
+    // calculate total...
+    const currentPhoneTotal = getElementValueById('phone-total');
+    const currentCaseTotal = getElementValueById('case-total');
+    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
 });
 
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
-    updatePhoneNumber(false);
-    
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
 });
